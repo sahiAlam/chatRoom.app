a85e551316c4e63b2ebe0bcf5a2a8f186c69f9b9
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-12-21 23:40:34 +0530

    another upload

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ http.listen(PORT, () => {
 app.use(express.static("public"));
 
 app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/index.html");
+  // response.sendFile(__dirname + "/index.html");
+  response.json({
+    hello: 'hii',
+    name: 'sahil'
+  });
 });
 
 const users = {};
