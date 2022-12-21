6b202ae151c8ddc5f98bd1be587bae7b6a7319f9
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-12-21 19:27:32 +0530

    fitst commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// Server file
+const { Socket } = require("dgram");
+const { response } = require("express");
+
+// create Express App
+const express = require("express");
+const { request } = require("http");
+const app = express();
+
+// Create Server and Port
+const http = require("http").createServer(app);
+
+const PORT = process.env.PORT || 8000;
+
+http.listen(PORT, () => {
+  console.log(`Listning on port ${PORT}`);
+});
+
+// Accessable folder and file path
+app.use(express.static("public"));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+const users = {};
+
+// Socket connection
+const io = require("socket.io")(http);
+
+io.on("connection", (socket) => {
+  // Message receiveed to server
+  socket.on("message", (msg) => {
+    // Message broadcast to all the users exccept You
+    socket.broadcast.emit("message", msg);
+  });
+});
---
+++ public/js/client.js
@@ -0,0 +1,110 @@
+// Socket instence
+const socket = io();
+
+// Taking user Name and load page
+let name;
+
+do {
+  name = prompt("Enter your name to connect");
+} while (!name);
+
+if (name != null) {
+  window.addEventListener("load", () => {
+    document.body.style.display = "block";
+  });
+}
+
+// Gatting Dom Elements
+const form = document.getElementById("messageForm");
+const messageInput = document.getElementById("messageInput");
+const messageBox = document.querySelector(".message_box");
+const container = document.querySelector(".chat_body_container");
+
+const welcome_msg = document.querySelector(".welcome_area");
+const welcomeName = document.querySelector("#welcomeName");
+
+var incommingAudio = new Audio("../audio/tung.mp3");
+var sentAudio = new Audio("../audio/sent.mp3");
+incommingAudio.volume = 0.2;
+incommingAudio.currentTime = 0.1;
+// sentAudio.volume = 0.01;
+// sentAudio.currentTime = 0.1;
+
+// Welcome message for the user
+welcomeName.innerHTML = name;
+welcome_msg.style.display = "block";
+
+let time;
+
+//submit message form
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  //Time
+  const createTime = new Date();
+  time = createTime.getHours() + ":" + createTime.getMinutes();
+
+  if (messageInput.value != "") {
+    sendMessage(messageInput.value);
+  }
+
+  messageInput.value = "";
+});
+
+// sending Meaasge
+function sendMessage(message) {
+  let msgObject = {
+    user: name,
+    msg: message,
+    time: time,
+  };
+
+  // Append
+  appendMessage(msgObject, "right_message_box");
+  scrollToBottom();
+
+  //Send to Server
+  socket.emit("message", msgObject);
+}
+
+// Append message to the DOM
+function appendMessage(message, type) {
+  let actualUser;
+  if (type === "right_message_box") {
+    actualUser = "You";
+  } else {
+    actualUser = message.user;
+  }
+
+  const mainDiv = document.createElement("div");
+  const className = type;
+  mainDiv.classList.add(className, "message");
+
+  const markup = `
+    <h2 class="userName">${actualUser}</h2>
+    <p class="userMsg">${message.msg}</P>
+    <h3 class="msgTime">${message.time}</h3>
+    `;
+
+  mainDiv.innerHTML = markup;
+
+  messageBox.appendChild(mainDiv);
+
+  // Audio
+  if (type === "left_message_box") {
+    incommingAudio.play();
+  } else {
+    // sentAudio.play();
+  }
+}
+
+// Recieve Message and appened to the DOM
+socket.on("message", (msg) => {
+  appendMessage(msg, "left_message_box");
+  scrollToBottom();
+});
+
+// Scroll Bottom
+const scrollToBottom = () => {
+  messageBox.scrollTop = messageBox.scrollHeight;
+};
