3a50f8f29185e47e4ee9ea1924dfa8015bb2bbc9
MD SAHI ALAM KHAN <ksahi4088@gmail.com>
2022-12-24 22:26:33 +0530

    vercel file updated

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Sahil')
+// Server file
+const { Socket } = require("dgram");
+const { response } = require("express");
+
+// create Express App
+const express = require("express");
+const { request } = require("http");
+const path = require("path");
+const app = express();
+
+// Create Server and Port
+const http = require("http").createServer(app);
+
+const PORT = process.env.PORT || 8000;
+
+http.listen(PORT, () => {
+  console.log(`Listning on port ${PORT}`);
+});
+
+// Accessable folder and file path
+app.use('/static', express.static(path.join(__dirname + '/public')));
+
+app.get("/", (request, response) => {
+  response.sendFile(path.join(__dirname + './public/index.html'));
+});
